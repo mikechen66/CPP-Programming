9d672a8358daad35b21a70a1101309ea600362a7
mikechen66 <875661606@qq.com>
2021-07-12 17:32:33 +0800

    Add files via upload

---
+++ CPP-Programming-Language/chapter12-functions/ex10.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+namespace ch12 {
+    using std::cout;
+    using std::endl;
+
+    class Circle;
+    class Square;
+    class Triangle;
+
+    class Shape {
+    public:
+        virtual bool intersect(Shape* s) = 0;
+        virtual bool intersect(Circle& s) = 0;
+        virtual bool intersect(Square& s) = 0;
+        virtual bool intersect(Triangle& s) = 0;
+    };
+
+    class Circle : public Shape {
+    public:
+        bool intersect(Shape* s) {
+            return s->intersect(*this);
+        }
+
+        bool intersect(Circle& s) {
+            cout << "Checking if Circle intersects with Circle" << endl;
+            return false;
+        }
+
+        bool intersect(Square& s) {
+            cout << "Checking if Circle intersects with Square" << endl;
+            return false;
+        }
+
+        bool intersect(Triangle& t) {
+            cout << "Checking if Circle intersects with Triangle" << endl;
+            return false;
+        }
+    };
+
+    class Square : public Shape {
+    public:
+        bool intersect(Shape* s) {
+            return s->intersect(*this);
+        }
+
+        bool intersect(Circle& s) {
+            return s.intersect(*this);
+        }
+
+        bool intersect(Square& s) {
+            cout << "Checking if Square intersects with Square" << endl;
+            return false;
+        }
+
+        bool intersect(Triangle& s) {
+            cout << "Checking if Square intersects with Triangle" << endl;
+            return false;
+        }
+    };
+
+    class Triangle : public Shape {
+    public:
+        bool intersect(Shape* s) {
+            return s->intersect(*this);
+        }
+
+        bool intersect(Circle& s) {
+            return s.intersect(*this);
+        }
+
+        bool intersect(Square& s) {
+            return s.intersect(*this);
+        }
+
+        bool intersect(Triangle& s) {
+            cout << "Checking if Triangle intersects with Triangle" << endl;
+            return false;
+        }
+    };
+
+    bool intersect(Shape* s1, Shape* s2) {
+        return s1->intersect(s2);
+    }
+}
+
+int main() {
+    using namespace std;
+    using namespace ch12;
+
+    Circle circle;
+    Square square;
+    Triangle triangle;
+
+    intersect(&circle, &square);
+    intersect(&circle, &triangle);
+    intersect(&square, &circle);
+    intersect(&square, &triangle);
+    intersect(&triangle, &circle);
+    intersect(&triangle, &square);
+}
+
+
+
+/* Output 、
+
+/*
+$g++ -o main *.cpp
+$main
+Checking if Circle intersects with Square
+Checking if Circle intersects with Triangle
+Checking if Circle intersects with Square
+Checking if Square intersects with Triangle
+Checking if Circle intersects with Triangle
+Checking if Square intersects with Triangle
+ */
---
+++ CPP-Programming-Language/chapter12-functions/ex12.cpp
@@ -0,0 +1,33 @@
+namespace ch12 {
+    class Monster;
+    class Object;
+
+    class Warrior {
+        virtual void move(int,int) = 0;
+        virtual void pick_up(Object*) = 0;
+        virtual void drop(Object*) = 0;
+        virtual void equip(Object*) = 0; // e.g. equip sword, shield, etc.
+        virtual void use(Object*) = 0; // e.g. scroll, potion
+        virtual void use(Object*,Object*) = 0; // e.g. use key on door
+        virtual void use(Object*,Monster*) = 0; // e.g. use scroll's spell on monster
+        virtual void attack(Monster*) = 0;
+        virtual void take_damage(int) = 0;
+    };
+
+    class Monster {
+        virtual void move(int,int) = 0;
+        virtual void attack(Warrior*) = 0;
+        virtual void take_damage(int) = 0;
+    };
+
+    class Object {
+        virtual void use(Object*) = 0; // e.g. use key on door (called by Warrior::Use(Object*,Object*))
+        virtual void use(Warrior*) = 0; // e.g. use on warrior (might decrease number of "charges" left, etc., called by Warrior::Use(Object*)0)
+        virtual void use(Monster*) = 0; // e.g. use on monster (called by Warrior::Use(Object*,Monster*))
+        virtual void degrade(int) = 0; // e.g. degrade a sword by 1 for each attack
+    };
+}
+
+int main() {
+    return 0;
+}
