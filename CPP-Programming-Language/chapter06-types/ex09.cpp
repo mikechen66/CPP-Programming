0563078a336e62a12b59be7ae812e94c0d59af5a
mikechen66 <875661606@qq.com>
2021-07-12 14:38:48 +0800

    Add files via upload

---
+++ CPP-Programming-Language/chapter06-types/ex09.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    struct mystruct_t { char *m; };
+    mystruct_t* p = new mystruct_t;
+    mystruct_t p1;
+    int* a[4];
+    int i = 0;
+
+    (*(p++));
+    cout << p << endl;
+    *(--p);
+    cout << p << endl;
+    //  (++(a--)); // not valid; prefix increment requires lvalue
+    ((int*)(p->m));
+    (*(p1.m));
+    (*(a[i]));
+}
+
+
+/* Output */
+
+/* 
+$g++ -o main *.cpp
+$main
+0x20b0c28
+0x20b0c20
+ */
