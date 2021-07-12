b40c7d32e123cc6e130b91703789d6a909caae10
mikechen66 <875661606@qq.com>
2021-07-12 17:29:43 +0800

    Add files via upload

---
+++ CPP-Programming-Language/chapter11-select-operations/ex04.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using std::ostream;
+
+class RINT {
+    int m_i;
+public:
+    explicit RINT(int i) : m_i(i) {}
+
+    RINT operator-() { return RINT(-m_i); }
+    RINT operator+() { return RINT(+m_i); }
+    RINT operator-(const RINT& x) { return RINT(m_i-x.m_i); }
+    RINT operator+(const RINT& x) { return RINT(m_i+x.m_i); }
+    RINT operator/(const RINT& x) { return RINT(m_i/x.m_i); }
+    RINT operator*(const RINT& x) { return RINT(m_i*x.m_i); }
+    RINT operator%(const RINT& x) { return RINT(m_i%x.m_i); }
+    RINT operator-(const int x) { return RINT(m_i-x); }
+    RINT operator+(const int x) { return RINT(m_i+x); }
+    RINT operator/(const int x) { return RINT(m_i/x); }
+    RINT operator*(const int x) { return RINT(m_i*x); }
+    RINT operator%(const int x) { return RINT(m_i%x); }
+
+    int val() const { 
+        return m_i; 
+    }
+};
+
+RINT operator-(const int x, const RINT& y) { return RINT(x - y.val()); }
+RINT operator+(const int x, const RINT& y) { return RINT(x + y.val()); }
+RINT operator/(const int x, const RINT& y) { return RINT(x / y.val()); }
+RINT operator*(const int x, const RINT& y) { return RINT(x * y.val()); }
+RINT operator%(const int x, const RINT& y) { return RINT(x % y.val()); }
+
+ostream& operator<<(ostream& out, const RINT& x) {
+    return out << x.val();
+}
+
+int main() {
+    using namespace std;
+
+    RINT x(2);
+    RINT y(3);
+    int z = 4;
+    cout << (-x) << endl;
+    cout << (+y) << endl;
+    cout << (y-x) << endl;
+    cout << (y+x) << endl;
+    cout << (y/x) << endl;
+    cout << (y*x) << endl;
+    cout << (y%x) << endl;
+
+    cout << (z-x) << endl;
+    cout << (x-z).val() << endl;
+    cout << (z+x) << endl;
+    cout << (x+z) << endl;
+    cout << (z/x) << endl;
+    cout << (x/z) << endl;
+    cout << (z*x) << endl;
+    cout << (x*z) << endl;
+    cout << (z%x) << endl;
+    cout << (x%z) << endl;
+}
+
+
+
+/* Output */
+
+/* 
+$g++ -o main *.cpp
+$main
+-2
+3
+1
+5
+1
+6
+1
+2
+-2
+6
+6
+2
+0
+8
+8
+0
+2
+ */
---
+++ CPP-Programming-Language/chapter11-select-operations/ex12.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+bool is_match(const string& str, const string& pattern, 
+        string::const_iterator pPos, string::const_iterator sPos) {
+    if (pPos == pattern.end() ||
+            (pPos + 1 == pattern.end() && *pPos == '*')) 
+        return true;
+    if (*pPos == '*') {
+        for (int idx = 0; idx < str.end() - sPos; idx++) {
+            string::const_iterator p = pPos+1;
+            string::const_iterator i = sPos+idx;
+            bool match = true;
+            while (i != str.end() && p != pattern.end()) {
+                if (*p == '*') 
+                    return is_match(str, pattern, p, i);
+                if (*i != *p) {
+                    match = false; 
+                    break;}
+                i++; p++;
+            }
+            if(match)
+                return i == str.end() && (p == pattern.end() || (p + 1 == pattern.end() && *p=='*'));
+        }
+        return false;
+    } else {
+        string::const_iterator p = pPos;
+        string::const_iterator i = sPos;
+        while(i != str.end() && p != pattern.end()) {
+            if (*p == '*') 
+                return is_match(str, pattern, p, i);
+            if (*i != *p) 
+                return false;
+            i++; p++;
+        }
+        return i == str.end() && (p == pattern.end() || (p+1 == pattern.end() && *p=='*'));
+    }
+}
+
+bool is_match(const string& str, const string& pattern) {
+    return is_match(str, pattern, pattern.begin(), str.begin());
+}
+
+int main() {
+    using namespace std;
+
+    string ex11("Hello world!");
+    cout << "Should be true: " << is_match(ex11,"*") << endl;
+    cout << "Should be true: " << is_match(ex11,"Hello*") << endl;
+    cout << "Should be true: " << is_match(ex11,"Hello world!") << endl;
+    cout << "Should be true: " << is_match(ex11,"*lo world!") << endl;
+    cout << "Should be true: " << is_match(ex11,"Hel*orld!") << endl;
+    cout << "Should be true: " << is_match(ex11,"Hel*o*d!") << endl;
+    cout << "Should be true: " << is_match(ex11,"*ello worl*") << endl;
+    cout << "Should be true: " << is_match(ex11,"*Hello world!") << endl;
+    cout << "Should be true: " << is_match(ex11,"Hello world!*") << endl;
+    cout << "Should be true: " << is_match(ex11,"*") << endl;
+
+    cout << "Should be false: " << is_match(ex11,"Hello world") << endl;
+    cout << "Should be false: " << is_match(ex11,"*Hello world") << endl;
+    cout << "Should be false: " << is_match(ex11,"Hello* world") << endl;
+    cout << "Should be false: " << is_match(ex11,"x*") << endl;
+    cout << "Should be false: " << is_match(ex11,"*x") << endl;
+    cout << "Should be false: " << is_match(ex11,"Hel*x*orld!") << endl;
+}
+
+
+
+/* Output */
+
+/*
+$g++ -o main *.cpp
+$main
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be true: 1
+Should be false: 0
+Should be false: 0
+Should be false: 0
+Should be false: 0
+Should be false: 0
+Should be false: 0
+ */
