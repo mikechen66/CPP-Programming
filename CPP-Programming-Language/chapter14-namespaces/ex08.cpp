b626132d88ba051a58aac6a57fed0d0455ff02d7
mikechen66 <875661606@qq.com>
2021-07-12 18:44:19 +0800

    Add files via upload

---
+++ CPP-Programming-Language/chapter14-namespaces/ex08.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+namespace ch14 {
+    // This exception class is patterned after the .NET System.Exception class.
+    // In C#, every exception must derive from this. It is missing some of the
+    // properties, such as the stack trace, and target method which require
+    // runtime support in .NET. However, the basic idea is the same.
+    //
+    // In this scenario, exceptions should derive from this class. Generic
+    // exceptions could also simply construct this class and throw it since it
+    // is not abstract. Generally, deriving classes should take similar
+    // constructor parameters to supply to the base, as well as any additional
+    // information specific to the exception.
+    //
+    // This pattern is considered advantageous in C# because it provides a base
+    // set of facilities for every error. For example, a suitable diagnostic
+    // error message, as well as the source of the error are provided for all
+    // exception types. This enables better debugging and logging scenarios.
+    // The use of the inner exception allows abstractions to preserve the
+    // exception stack and is useful for diagnosing the root cause of a failure
+    // without multiplying the number of exceptions a consumer must handle.
+    //
+    // One disadvantage is that there might be more overhead than throwing a
+    // simple empty exception because the fields need to be copied. However,
+    // since exceptions are exceptional, performance should not be a primary
+    // concern. Another disadvantage is that slicing (throwing a specific
+    // exception but catching Exception) without using a pointer or reference
+    // would cause the incorrect write method to be used. For this reason, it
+    // is better to catch by reference using this pattern.
+    class Exception {
+        std::string m_message;
+        Exception* m_inner;
+        std::string m_source;
+
+    public:
+        Exception() : m_inner(0) {}
+        Exception(const std::string& message) : m_message(message), m_inner(0) {}
+        Exception(const std::string& message, Exception* inner)
+            : m_message(message), m_inner(inner) {}
+
+        virtual void write(std::ostream& output) {
+            output << m_message << std::endl;
+            if (m_inner) {
+                m_inner->write(output);
+            }
+        }
+
+        const std::string& message() const { return m_message; }
+        const std::string& source() const { return m_source; }
+        Exception * const inner() const { return m_inner; }
+    };
+
+    std::ostream& operator<<(std::ostream& output, Exception& ex) {
+        ex.write(output);
+        return output;
+    }
+
+    class SpecificException : public Exception {
+    public:
+        SpecificException() : Exception() {}
+        SpecificException(const std::string& message) : Exception(message) {}
+        SpecificException(const std::string& message, Exception* inner) : Exception(message,inner) {}
+
+        void write(std::ostream& output) {
+            std::cout << "SpecificException is being thrown:" << std::endl;
+            Exception::write(output);
+        }
+    };
+}
+
+void f() {
+    throw ch14::SpecificException("a specific error occurred");
+}
+
+int main() {
+    using namespace std;
+    using namespace ch14;
+
+    try {
+        f();
+    }
+    catch(Exception& ex) {
+        cout << ex;
+    }
+
+    return 0;
+}
+
+
+
+/* Output */
+
+/*
+$g++ -o main *.cpp
+$main
+SpecificException is being thrown:
+a specific error occurred
+ */
---
+++ CPP-Programming-Language/chapter14-namespaces/ex10.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+namespace ch14 {
+    template<class Targ>
+    class Callback {
+    public:
+        typedef void (CallbackFunc)(Targ);
+
+    private:
+        CallbackFunc* func;
+
+    public:
+        class FunctionNullError {};
+
+        Callback() : func(0) {}
+        explicit Callback(CallbackFunc* funcPtr) : func(funcPtr) {}
+
+        void invoke(Targ) const;
+    };
+
+    template<>
+    class Callback<void> {
+    public:
+        typedef void (CallbackFunc)();
+
+    private:
+        CallbackFunc* func;
+
+    public:
+        class FunctionNullError {};
+
+        Callback() : func(0) {}
+        explicit Callback(CallbackFunc* funcPtr) : func(funcPtr) {}
+
+        void invoke() const;
+    };
+
+    template<class Targ>
+    void Callback<Targ>::invoke(Targ arg) const {
+        if (!func) 
+            throw FunctionNullError();
+        // Let the invoked function throw any exception; don't trap it
+        func(arg);
+    }
+
+    void Callback<void>::invoke() const {
+        if (!func) 
+            throw FunctionNullError();
+        // Let the invoked function throw any exception; don't trap it
+        func();
+    }
+}
+
+void f() {
+    std::cout << "f was invoked" << std::endl;
+}
+
+void g(int x) {
+    std::cout << "g was invoked with " << x << std::endl;
+}
+
+int main() {
+    using namespace std;
+    using namespace ch14;
+
+    Callback<void> c(&f);
+    c.invoke();
+
+    Callback<int> c2(&g);
+    c2.invoke(1);
+
+    return 0;
+}
+
+
+
+/* Output */
+
+/* 
+$g++ -o main *.cpp
+$main
+f was invoked
+g was invoked with 1
+ */
