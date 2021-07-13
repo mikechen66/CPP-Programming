128c808541ea32ddfb74cc7ce799d68d6402671a
mikechen66 <875661606@qq.com>
2021-07-13 16:16:22 +0800

    Add files via upload

---
+++ CPP-Programming-Language/chapter10-expression/ex16/lexer.h
@@ -0,0 +1,50 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <iostream>
+#include <queue>
+
+namespace ch10
+{
+    using std::string;
+    using std::istream;
+    using std::queue;
+
+    struct Token
+    {
+        enum TokenValue {
+            NAME, NUMBER, END, FUNC = '%',
+            PLUS='+', MINUS='-', MUL='*', DIV='/',
+            PRINT=';', ASSIGN='=', LP='(', RP=')',
+            SEP=','
+        };
+
+        TokenValue Type;
+        double NumberValue;
+        string StringValue;
+
+        Token(TokenValue type) : Type(type) {}
+        Token(TokenValue type, double value) : Type(type), NumberValue(value) {}
+        Token(TokenValue type, string value) : Type(type), StringValue(value) {}
+    };
+
+    class Lexer
+    {
+        istream* input;
+        int line;
+        queue<Token> m_queue;
+
+        Token read_token();
+
+    public:
+        Lexer(istream* input) : input(input), line(1) {}
+        Token pop();
+        Token peek();
+        void push(Token);
+        int get_line() { return line; }
+        void clear() { m_queue = std::queue<Token>(); }
+    };
+}
+
+#endif
